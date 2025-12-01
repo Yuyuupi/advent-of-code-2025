fb34311d09a98547d5e05c7c9d8d3ac81ec218ac
Yuyupi Yupi <yuyupi@protonmail.com>
2025-12-01 10:55:44 +0100

    Solution for day-1

--- day-1/src/space/yupi/Main.java
+++ day-1/src/space/yupi/Main.java
@@ -1,4 +1,64 @@
 package space.yupi;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
+
+    private static int dial = 50;
+    private static int counter = 0;
+    private static int counter2 = 0;
+
+    static void main() {
+        File input = new File("input.txt");
+
+        try(Scanner scanner = new Scanner(input)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                rotateDial(line);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+
+        System.out.println(dial);
+        System.out.println(counter);
+        System.out.println(counter2);
+    }
+
+
+    private static void rotateDial(String direction) {
+        if (direction.startsWith("R")){
+            for (int i = 0; i < Integer.parseInt(direction.substring(1)); i++) {
+                if (dial == 99) {
+                    dial = 0;
+                } else {
+                    dial += 1;
+                }
+
+                if (dial == 0) {
+                    counter2 += 1;
+                }
+            }
+        } else if (direction.startsWith("L")) {
+            for (int i = 0; i < Integer.parseInt(direction.substring(1)); i++) {
+                if (dial == 0) {
+                    dial = 99;
+                } else {
+                    dial -= 1;
+                }
+
+                if (dial == 0) {
+                    counter2 += 1;
+                }
+            }
+        }
+
+        if (dial == 0){
+            counter += 1;
+        }
+
+    }
+
 }
