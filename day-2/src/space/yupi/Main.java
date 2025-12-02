8a6a7f276053fb29757674814d83ee462f863672
Yuyupi Yupi <yuyupi@protonmail.com>
2025-12-02 10:02:47 +0100

    Solution for day-2

--- day-2/src/space/yupi/Main.java
+++ day-2/src/space/yupi/Main.java
@@ -1,7 +1,38 @@
 package space.yupi;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
     void main() {
+        File inputFile = new File("input-day-2.txt");
+        String input;
+        String[] ranges;
+        long pw = 0;
+        long pw2 = 0;
 
+        try {
+            Scanner scanner = new Scanner(inputFile);
+            input = scanner.next();
+            ranges = input.split(",");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (String range : ranges) {
+            String[] bounds = range.split("-");
+            for (long i = Long.parseLong(bounds[0]); i <= Long.parseLong(bounds[1]); i++) {
+                if(String.valueOf(i).matches("^(.+)\\1$")) {
+                    pw += i;
+                }
+                if(String.valueOf(i).matches("^(.+)\\1+$")) {
+                    pw2 += i;
+                }
+            }
+        }
+
+        System.out.println(pw);
+        System.out.println(pw2);
     }
 }
